f02f9128de9986e2a877546ce845708c86a58d7b
marceloleal <marcelosleal@outlook.com>
2022-11-28 17:03:56 -0300

    Adicionando o exercício Two Fer

---
+++ java/two-fer/src/main/java/Twofer.java
@@ -0,0 +1,10 @@
+public class Twofer {
+    public String twofer(String name) {
+
+        String nome = name == null ? "you" : name;
+
+        return "One for " + nome + ", one for me.";
+
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
